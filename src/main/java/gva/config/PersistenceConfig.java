3b66b47193faba2809da44a48e224d6f802acf26
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2017-07-01 08:25:33 +0300

    external persistence configuration

---
+++ src/main/java/gva/config/PersistenceConfig.java
@@ -0,0 +1,62 @@
+package gva.config;
+
+import gva.model.Answer;
+import gva.model.Question;
+import gva.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:persistence.properties")
+public class PersistenceConfig {
+    private Environment env;
+
+    @Autowired
+    public PersistenceConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        Properties properties = new Properties();
+        properties.put(AvailableSettings.SHOW_SQL, env.getProperty("hibernate.show_sql"));
+        properties.put(AvailableSettings.DIALECT, env.getProperty("hibernate.dialect"));
+        properties.put(AvailableSettings.ENABLE_LAZY_LOAD_NO_TRANS, env.getProperty("hibernate.enable_lazy_load_no_trans"));
+        properties.put(AvailableSettings.HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+        sessionFactoryBean.setHibernateProperties(properties);
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setAnnotatedClasses(Answer.class, Question.class, User.class);
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("connection.driver.class"));
+        dataSource.setUrl(env.getProperty("connection.url"));
+        dataSource.setUsername(env.getProperty("connection.username"));
+        dataSource.setPassword(env.getProperty("connection.password"));
+        return dataSource;
+    }
+}
