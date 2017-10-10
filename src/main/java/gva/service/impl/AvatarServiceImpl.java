34edd7d6fafa8404435b5692a0e225bc9ba1f1f6
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2017-10-10 18:48:27 +0300

    create directories for avatars uploading on post-construct

--- src/main/java/gva/service/impl/AvatarServiceImpl.java
+++ src/main/java/gva/service/impl/AvatarServiceImpl.java
@@ -1,16 +1,29 @@
 package gva.service.impl;
 
 import gva.service.AvatarService;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
 public class AvatarServiceImpl implements AvatarService {
+
     @Value("${avatar.path}")
     private String avatarPath;
+
+    @PostConstruct
+    private void init() {
+        try {
+            FileUtils.forceMkdir(new File(avatarPath));
+        } catch (IOException e) {
+            System.err.println("Failed to create directories for avatars uploading");
+        }
+    }
 
     @Override
     public void store(String fileName, byte[] fileContent) throws IOException {
